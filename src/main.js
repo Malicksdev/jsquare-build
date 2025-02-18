32e476dbd8d32ed72d4fbaa1026aed977a9c7318
Malick Shillah <91393833+Malicksdev@users.noreply.github.com>
2025-02-18 14:28:25 +0300

    updated button

--- src/main.js
+++ src/main.js
@@ -2,15 +2,5 @@ import './scene.js';
 import './style.css';
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    const exploreButton = document.querySelector('.explore-button');
-    
-    // Optional: log to ensure the button is found
-    console.log('Explore button found:', exploreButton);
-  
-    exploreButton.addEventListener('click', () => {
-      console.log('Explore button clicked!');
-      window.location.href = 'explore.html';
-    });
-  });
+
   
